e15ac21b20162852c14466b1d5283da6a7db566e
ColeKainz <cole.r.kainz@gmail.com>
2021-01-31 11:38:18 -0600

    Fix misimplementation.
    Reverted MarketSummaryDelta.kt delta to deltas

--- src/main/kotlin/com/bushka/bittrex/model/sockets/MarketSummaryDelta.kt
+++ src/main/kotlin/com/bushka/bittrex/model/sockets/MarketSummaryDelta.kt
@@ -4,5 +4,5 @@ import com.bushka.bittrex.model.markets.MarketSummary
 
 data class MarketSummaryDelta(
         override val sequence: Int,
-        val delta: MarketSummary
+        val deltas: List<MarketSummary>
 ) : Sequenced
